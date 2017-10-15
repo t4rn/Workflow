e5eb0fbd0603a78503d85feba9376948176789a6
t4rn <kwitkowski.it@gmail.com>
2017-10-15 17:59:36 +0200

    Solution structure

---
+++ Workflow.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Workflow.Web.Startup))]
+namespace Workflow.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
